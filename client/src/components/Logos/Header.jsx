32f84d907077c5ecd0e77a9273b0f2b35800fb8d
Danius98 <luisdanielarciniegasbarco@gmail.com>
2021-09-28 16:28:08 -0500

    Demo Header

---
+++ client/src/components/Logos/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'Header.css'
+
+export default function Header() {
+    return (
+        <div>
+            <header>
+                <h6 className="h6">Maxi por fa no me mates. Header de prueba</h6>
+            </header>
+        </div>
+    )
+}
