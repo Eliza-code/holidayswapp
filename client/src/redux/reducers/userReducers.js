ae6c45aadfd3c25360e2855fb0f14326ff134ee8
Danius98 <luisdanielarciniegasbarco@gmail.com>
2021-09-29 13:29:14 -0500

    announcements mock in initial state

--- client/src/redux/reducers/userReducers.js
+++ client/src/redux/reducers/userReducers.js
@@ -1,7 +1,68 @@
 const initialState =  {
     home: [],
     announcements: [{
-       
+        id: 1,
+        Dueño: "Silvio",
+        Ciudad: "Roma",
+        País: "Italy",
+        Rating: 5,
+        Puntos: 300
+    },
+    {
+        id: 2,
+        Dueño: "Maria",
+        Ciudad: "Roma",
+        País: "Italy",
+        Rating: 5,
+        Puntos: 200
+    },
+    {
+        id: 3,
+        Dueño: "Silvia",
+        Ciudad: "Roma",
+        País: "Italy",
+        Rating: 5,
+        Puntos: 400
+    },
+    {
+        id: 4,
+        Dueño: "Silvana",
+        Ciudad: "Roma",
+        País: "Italy",
+        Rating: 5,
+        Puntos: 100
+    },
+    {
+        id: 5,
+        Dueño: "Silvestre",
+        Ciudad: "Roma",
+        País: "Italy",
+        Rating: 5,
+        Puntos: 400
+    },
+    {
+        id: 6,
+        Dueño: "Sandro",
+        Ciudad: "Roma",
+        País: "Italy",
+        Rating: 5,
+        Puntos: 300
+    },
+    {
+        id: 7,
+        Dueño: "Sofia",
+        Ciudad: "Roma",
+        País: "Italy",
+        Rating: 5,
+        Puntos: 300
+    },
+    {
+        id: 8,
+        Dueño: "Sandro",
+        Ciudad: "Roma",
+        País: "Italy",
+        Rating: 5,
+        Puntos: 400
     }]
 }
 
