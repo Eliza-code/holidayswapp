2af8793be64289f962fb0a453a1a612f0151fb4b
Danius98 <luisdanielarciniegasbarco@gmail.com>
2021-09-28 19:02:06 -0500

    feat/simulating redux shit

---
+++ client/src/redux/actions/userActions.js
@@ -0,0 +1,8 @@
+export const HOGARES = "HOGARES"
+
+export function Hogares(payload) {
+    return {
+        type: HOGARES,
+        payload
+    }
+}
--- client/src/redux/reducers/userReducers.js
+++ client/src/redux/reducers/userReducers.js
@@ -67,6 +67,11 @@ const initialState =  [{
 
 const userReducer = (state=initialState, action) => {
     switch (action.type){
+        case "HOGARES":
+        return {
+            ...state,
+            Hogares
+        }
         
         default: 
         return state;
