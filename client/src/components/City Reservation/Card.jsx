4fdbbc720d8445bde37db14c0a5846b02027e9c5
Danius98 <luisdanielarciniegasbarco@gmail.com>
2021-09-28 22:58:53 -0500

    feat/little additions to Card components

--- client/src/components/City Reservation/Card.jsx
+++ client/src/components/City Reservation/Card.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import './Card.css';
 
-export default function CountryCard(props) {
+export default function AnnouncementCard(props) {
   // acá va tu código
-  const {codigo, nombre, bandera, continente, activities} = props;
+  const {id, owner, image, country, city} = props;
   return (
   <div className="card2">
-    <Link to={`/Country/${codigo}`}>
+    <Link to={`/Anuncio/${id}`}>
     <li className="list-item">
-      <img width={100}src={bandera} alt="No imagen"/>
+      <img width={100}src={image[0]} alt="No imagen"/>
       <div className="align">
-  <h4 className="name">{nombre}</h4>
-  <h4>{continente}</h4>
-  <h4>{activities}</h4>
+  <h4 className="name">{owner}</h4>
+  <h4>{country}</h4>
+  <h4>{city}</h4>
+  <h4>{points}</h4>
   </div>
     </li>
     </Link>
