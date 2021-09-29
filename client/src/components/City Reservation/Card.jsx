403ddefaa624cf218fbacc52fadee89d0c1ef3b4
Danius98 <luisdanielarciniegasbarco@gmail.com>
2021-09-29 14:37:32 -0500

    adjusting card mock to show reducer mock

--- client/src/components/City Reservation/Card.jsx
+++ client/src/components/City Reservation/Card.jsx
@@ -4,17 +4,18 @@ import './Card.css';
 
 export default function AnnouncementCard(props) {
   // acá va tu código
-  const {id, owner, image, country, city} = props;
+  const {id, Dueño, Ciudad, País, Rating, Puntos} = props;
   return (
   <div className="card2">
     <Link to={`/Anuncio/${id}`}>
     <li className="list-item">
-      <img width={100}src={image[0]} alt="No imagen"/>
+      {/*<img width={100}src={image[0]} alt="No imagen"/>*/}
       <div className="align">
-  <h4 className="name">{owner}</h4>
-  <h4>{country}</h4>
-  <h4>{city}</h4>
-  <h4>{points}</h4>
+  <h4 className="name">{Dueño}</h4>
+  <h4>{Ciudad}</h4>
+  <h4>{País}</h4>
+  <h4>{Rating}</h4>
+  <h4>{Puntos}</h4>
   </div>
     </li>
     </Link>
