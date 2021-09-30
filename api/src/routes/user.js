3a4bef7302c742a45e4f222023921f874e1f9c50
unknown <mauriciodamianfollonier@hotmail.com>
2021-09-30 16:24:19 -0300

    Otro conflicto resulto

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-const router = require('express').Router();
-const express = require('express');
-const {User} = require('./../db');
-const {Announcement} = require('./../db');
-
-router.use(express.json());
-
-router.post('/', async (req, res) => {
-    let user = req.body;
-    try{
-     const newUser = await User.create({
-        ...user,
-        email: user.email.toLowerCase()
-    })
-    return res.json(newUser)
-    } catch (error) {
-      conaole.log(error)
-    }
-
-});
-
-router.get("/", async (req, res) => {
-    
-    try {
-      let allUsers = await User.findAll({
-          include: {
-              model: Announcement
-          }
-      });
-      allUsers ? res.json(allUsers) : res.sendStatus(404);
-    } catch (error) {
-      res.status(400).send(error);
-    }
-  });
-=======
 const { Router } = require('express');
 const express = require('express');
 const {User} =require('../../src/db')
@@ -53,6 +17,5 @@ router.post('/', createUser
 
 
 
->>>>>>> 6c600863fa966ec00923b64eacc2c28c61156fb4
 
 module.exports = router;
