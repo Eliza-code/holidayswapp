d88e8a745baf6a89a1483701ffb518de34f2db73
Bellantra <yamilapaez2@hotmail.com>
2021-10-10 17:38:52 +0200

    Agregado opciones de la vista de My Booking

--- client/src/components/Booking/MyBookings.jsx
+++ client/src/components/Booking/MyBookings.jsx
@@ -1,21 +1,70 @@
+import {
+  BottomNavigation,
+  Divider,
+  Grid,
+  List,
+  ListItemButton,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
 import "../Booking/booking.css";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import NavBar from "../NavBar/NavBar";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  divider: {
+    marginLeft:30,
+  },
+}));
 
 const MyBookings = () => {
+  const classes = useStyles();
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
+  const handleListItemClick = (event, index) => {
+    setSelectedIndex(index);
+  };
   return (
-    <div>
-      <div className="headerNav">
+    <Grid className="headerNav" container xs={12}>
+      <Grid item xs={12} spacing={0}>
         <Header />
         <NavBar />
-      </div>
+      </Grid>
 
-      <div>
+      <Grid container>
+          <Grid item xs={12} align="center"  spacing={0}>
+            <Typography variant="h3" >My Bookings</Typography>
+          </Grid>
+        
+        <Grid item>
+          <List>
+            <ListItemButton
+              selected={selectedIndex === 0}
+              onClick={(event) => handleListItemClick(event, 0)}
+            >
+              <ListItemText primary="Requests sent" />
+            </ListItemButton>
+            <ListItemButton
+              selected={selectedIndex === 1}
+              onClick={(event) => handleListItemClick(event, 1)}
+            >
+              <ListItemText primary="Requests Received" />
+            </ListItemButton>
+          </List>
+        </Grid>
+        <Divider className={classes.divider} orientation="vertical" flexItem />
+
+        <Grid item xs={3 / 4}></Grid>
+      </Grid>
+
+      <Grid item xs={12}>
         <Footer />
-      </div>
-    </div>
+      </Grid>
+    </Grid>
   );
 };
 
