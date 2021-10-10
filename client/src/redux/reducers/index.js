b480962c5a2376f6fc2caa90a8677a42e8775b48
MaxiBelli <maximiliano_belli@hotmail.com>
2021-10-10 12:24:54 -0300

    actions and reducers review

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,9 +1,13 @@
 import userReducer from '../reducers/userReducers';
 import postReducer from '../reducers/postReducers';
+import favouriteReducer from '../reducers/favouriteReducer';
+import reviewReducer from '../reducers/reviewReducer';
 import { combineReducers } from 'redux';
 
 export default combineReducers({
     userReducer,
     postReducer,
+    favouriteReducer,
+    reviewReducer
 });
 
