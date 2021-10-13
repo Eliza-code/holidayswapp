382fa1a711a32c0f61c529dbcb88012a88ac3e5c
MaxiBelli <maximiliano_belli@hotmail.com>
2021-10-13 20:33:12 -0300

    Orders and Filters Announcements

---
+++ api/src/controllers/announcement/ordersFiltersAnnouncement.js
@@ -0,0 +1,54 @@
+const { Announcement, User } = require("../../db");
+const { Op } = require("sequelize");
+
+module.exports = async (req, res, next) => {
+  const { name } = req.query;
+  console.log(name);
+  if (name) {
+      try {
+      const announcementLocation = await Announcement.findAll({
+        where: {
+          [Op.or]: [
+            { city: { [Op.iLike]: `%${name}%` } },
+            { country: { [Op.iLike]: `%${name}%` } },
+          ],
+        },
+      });
+
+      announcementLocation.length
+        ? res.status(200).json(announcementLocation)
+        : res
+            .status(404)
+            .send("The city or country with that name was not found");
+          } 
+          catch(err){
+                next(err);
+            res.json(err);
+          }
+          } else if (req.query.filter) {
+            try {
+              let announcementFilter = await Announcement.findAll({
+                where: {
+                  type: req.query.filter,
+                },
+                // order: [["surfaceM2", req.query.order]],
+              });
+              return res.json(announcementFilter);
+            } 
+            catch(err){
+                  next(err);
+              res.json(err);
+            }
+          } else {
+            try {
+              let announcementRating = await Announcement.findAll({
+                order: [["surfaceM2", req.query.order]],
+              });
+              return res.json(announcementRating);
+            } 
+            catch(err){
+                  next(err);
+              res.json(err);
+            }
+          }
+        };
--- client/src/redux/reducers/postReducers.js
+++ client/src/redux/reducers/postReducers.js
@@ -4,7 +4,8 @@ const initialState = {
   homeInfo: [],
   searchResults: [],
   houses: [],
-  currentPostId: JSON.parse(window.localStorage.getItem("currentPost"))
+  currentPostId: JSON.parse(window.localStorage.getItem("currentPost")),
+  allSearchResults: [],
 };
 
 const postReducer = (state = initialState, action) => {
@@ -25,12 +26,63 @@ const postReducer = (state = initialState, action) => {
       return {
         ...state,
         searchResults: action.payload,
+        allSearchResults: action.payload,
       };
-    case types.POST_ANNOUNCEMENT: 
-    return {
-      ...state
-    };
-   
+    case types.POST_ANNOUNCEMENT:
+      return {
+        ...state,
+      };
+    case types.ORDER_BY_POINTS:
+      let sortedPoints =
+        action.payload === "mayor"
+          ? state.searchResults.sort(function (a, b) {
+              return b.points - a.points;
+            })
+          : state.searchResults.sort(function (a, b) {
+              return a.points - b.points;
+            });
+
+      return {
+        ...state,
+        searchResults: sortedPoints,
+      };
+    case types.ORDER_BY_RATING:
+      let sortedRating =
+        action.payload === "mayor"
+          ? state.searchResults.sort(function (a, b) {
+              return b.rating - a.rating;
+            })
+          : state.searchResults.sort(function (a, b) {
+              return a.rating - b.rating;
+            });
+
+      return {
+        ...state,
+        searchResults: sortedRating,
+      };
+    case types.FILTER_BY_TYPE:
+      const allResults = state.allSearchResults;
+      const typeFiltered =
+        action.payload === "All"
+          ? allResults
+          : allResults.filter((el) => el.type === action.payload);
+
+      return {
+        ...state,
+        searchResults: typeFiltered,
+      };
+
+    case types.FILTER_BY_GUEST:
+      const allSearch = state.allSearchResults;
+      const guestFiltered =
+        action.payload === "All"
+          ? allSearch
+          : allSearch.filter((el) => el.sleeps === action.payload);
+
+      return {
+        ...state,
+        searchResults: guestFiltered,
+      };
     default:
       return state;
   }
